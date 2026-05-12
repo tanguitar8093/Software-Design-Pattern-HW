74f55da03571b29cbf24dd35fd72a7f936921c5a
allen.tan <allen.tan@mitaccomputing.com>
2026-05-12 18:11:21 +0800

    doc: 移動資料夾

--- hmoework5/v2/models/observer.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class DeathObserver(ABC):
-    @abstractmethod
-    def on_target_dead(self, dead_unit, remaining_mp):
-        pass
--- hmoework5/v2/models/troop.py
+++
@@ -1,8 +0,0 @@
-class Troop:
-    def __init__(self, units):
-        self.units = units
-        for u in self.units:
-            u.troop = self
-
-    def is_annihilated(self):
-        return all(u.is_dead for u in self.units)
